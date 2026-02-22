830927c60b1e23ac286f2c9ef0478d4de646e5e2
Andrew Simachev <andrew.simachev@gmail.com>
2026-02-22 12:08:52 +0100

    add menu participant loader for blank images

--- src/ts/component/menu/participant.tsx
+++ src/ts/component/menu/participant.tsx
@@ -1,15 +1,18 @@
-import React, { forwardRef, useEffect } from 'react';
+import React, { forwardRef, useRef, useState, useEffect } from 'react';
+import $ from 'jquery';
 import { observer } from 'mobx-react';
-import { ObjectName, ObjectDescription, Label, IconObject, EmptySearch, Button } from 'Component';
+import { ObjectName, ObjectDescription, Label, IconObject, EmptySearch, Button, Loader } from 'Component';
 import { I, U, S, translate, Action, analytics } from 'Lib';
 
 const MenuParticipant = observer(forwardRef<I.MenuRef, I.Menu>((props: I.Menu, ref: any) => {
-	
+
 	const { param, close } = props;
 	const { data } = param;
 	const { object } = data;
 	const { space } = S.Common;
 	const { account } = S.Auth;
+	const iconRef = useRef(null);
+	const [ isLoaded, setIsLoaded ] = useState(!object?.iconImage);
 	const oneToOne = U.Space.getList().filter(it => it.isOneToOne && (it.oneToOneIdentity == object.identity))[0];
 	const showButton = (oneToOne && oneToOne.targetSpaceId != space) || !oneToOne || (object.identity == account.id);
 	const globalName = object.globalName || U.String.shortMask(object.identity, 6);
@@ -47,6 +50,34 @@ const MenuParticipant = observer(forwardRef<I.MenuRef, I.Menu>((props: I.Menu, r
 
 	useEffect(() => load(), []);
 
+	useEffect(() => {
+		if (isLoaded) {
+			return;
+		};
+
+		const node = $(iconRef.current);
+		const img = node.find('.iconImage');
+
+		if (!img.length) {
+			setIsLoaded(true);
+			return;
+		};
+
+		const onImageLoad = () => setIsLoaded(true);
+
+		img.on('load', onImageLoad);
+		img.on('error', onImageLoad);
+
+		if ((img.get(0) as HTMLImageElement)?.complete) {
+			setIsLoaded(true);
+		};
+
+		return () => {
+			img.off('load', onImageLoad);
+			img.off('error', onImageLoad);
+		};
+	});
+
 	return object ? (
 		<>
 			<div className="head">
@@ -55,7 +86,10 @@ const MenuParticipant = observer(forwardRef<I.MenuRef, I.Menu>((props: I.Menu, r
 					onClick={() => U.Common.copyToast(translate('blockFeaturedIdentity'), object.identity)}
 				/>
 			</div>
-			<IconObject object={object} size={128} />
+			<div ref={iconRef} className={[ 'iconWrapper', (isLoaded ? 'isLoaded' : '') ].join(' ')}>
+				<Loader type={I.LoaderType.Loader} />
+				<IconObject object={object} size={128} />
+			</div>
 			<div className="nameWrapper">
 				<ObjectName object={object} withBadge={true} />
 			</div>
